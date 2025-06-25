c12803e7a7eff8efc9b2943ceabf399f419408c4
Yashwant Moharil <yashmoharil@gmail.com>
2025-06-25 17:29:44 -0500

    done

--- server/index.js
+++ server/index.js
@@ -59,6 +59,7 @@ app.post("/login", async(req, res) => {
     }
 })
 
-app.listen(3001, () => {
-    console.log("Server running 30001");
-})
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
