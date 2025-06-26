f11c19f185eafd09997541e9f8d65de4b5788d77
Yashwant Moharil <yashmoharil@gmail.com>
2025-06-26 18:47:20 -0500

    done

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -1,32 +1,37 @@
 import React, { useState } from 'react'
 import axios from "axios"
 import Cookies from "universal-cookie"
+import infoIcon from "../Assets/Images/informationIcon.png"
 
-function Login({setIsAuth}) {
+function Login({ setIsAuth }) {
   const [userName, setUserName] = useState("");
   const [passWord, setPassWord] = useState("");
   const cookies = new Cookies();
   const logInUser = () => {
-       if(userName === "" || passWord === "") return alert("Username or Password is missing");
+    if (userName === "" || passWord === "") return alert("Username or Password is missing");
 
-       axios.post(`${process.env.REACT_APP_BACKEND_BASE_URL}/login`, {userName, passWord}).then(res => {
-        const {token, userId, firstName, lastName, userName} = res.data;
-        cookies.set("token", token);
-        cookies.set("userId", userId);
-        cookies.set("firstName", firstName);
-        cookies.set("lastName", lastName);
-        cookies.set("userName", userName);
-        setIsAuth(true);
+    axios.post(`${process.env.REACT_APP_BACKEND_BASE_URL}/login`, { userName, passWord }).then(res => {
+      const { token, userId, firstName, lastName, userName } = res.data;
+      cookies.set("token", token);
+      cookies.set("userId", userId);
+      cookies.set("firstName", firstName);
+      cookies.set("lastName", lastName);
+      cookies.set("userName", userName);
+      setIsAuth(true);
 
-      })
+    })
   }
   return (
+    <>
       <div className='auth-container '>
-    <label>Login</label>
-    <input placeholder='Username' onChange={(event) => {setUserName(event.target.value)} }/>
-    <input placeholder='Password' onChange={(event) => {setPassWord(event.target.value)} }/>
-    <button onClick={logInUser}>Login</button>
-    </div>
+        < img className='infoIcon1' src={infoIcon} title="The backend is hosted on free servers, so Login/Signup takes ~15s on first use." />
+        <label>Login</label>
+        <input placeholder='Username' onChange={(event) => { setUserName(event.target.value) }} />
+        <input placeholder='Password' onChange={(event) => { setPassWord(event.target.value) }} />
+        <button onClick={logInUser}>Login</button>
+      </div>
+    </>
+
   )
 }
 
--- client/src/Components/SignUp.jsx
+++ client/src/Components/SignUp.jsx
@@ -1,33 +1,39 @@
 import React, { useState } from 'react'
 import axios from "axios"
 import Cookies from "universal-cookie"
+import infoIcon from "../Assets/Images/informationIcon.png"
 
-function Signup({setIsAuth}) {
-    const cookies = new Cookies();
-    const [user, setUser] = useState(null)
-    const signUpUser = () => {
-      axios.post(`${process.env.REACT_APP_BACKEND_BASE_URL}/signup`, user).then(res => {
-        const {token, userId, firstName, lastName, userName, hashedPassword} = res.data;
-        
-        cookies.set("token", token);
-        cookies.set("userId", userId);
-        cookies.set("firstName", firstName);
-        cookies.set("lastName", lastName);
-        cookies.set("userName", userName);
-        cookies.set("hashedPassword", hashedPassword);
-        setIsAuth(true);
-      })
 
-    }
+function Signup({ setIsAuth }) {
+  const cookies = new Cookies();
+  const [user, setUser] = useState(null)
+  const signUpUser = () => {
+    axios.post(`${process.env.REACT_APP_BACKEND_BASE_URL}/signup`, user).then(res => {
+      const { token, userId, firstName, lastName, userName, hashedPassword } = res.data;
+
+      cookies.set("token", token);
+      cookies.set("userId", userId);
+      cookies.set("firstName", firstName);
+      cookies.set("lastName", lastName);
+      cookies.set("userName", userName);
+      cookies.set("hashedPassword", hashedPassword);
+      setIsAuth(true);
+    })
+
+  }
   return (
-    <div className='auth-container '>
-    <label>SignUp</label>
-    <input placeholder='First Name' onChange={(event) => {setUser({...user, firstName : event.target.value})} }/>
-    <input placeholder='Last Name' onChange={(event) => {setUser({...user, lastName : event.target.value})} }/>
-    <input placeholder='Username' onChange={(event) => {setUser({...user, userName : event.target.value})} }/>
-    <input placeholder='Password' onChange={(event) => {setUser({...user, passWord : event.target.value})} }/>
-    <button onClick={signUpUser}>Sign Up</button>
-    </div>
+    <>
+      <div className='auth-container '>
+        <img className='infoIcon2' src={infoIcon} title="The backend is hosted on free servers, so Login/Signup takes ~15s on first use." />
+        <label>SignUp</label>
+        <input placeholder='First Name' onChange={(event) => { setUser({ ...user, firstName: event.target.value }) }} />
+        <input placeholder='Last Name' onChange={(event) => { setUser({ ...user, lastName: event.target.value }) }} />
+        <input placeholder='Username' onChange={(event) => { setUser({ ...user, userName: event.target.value }) }} />
+        <input placeholder='Password' onChange={(event) => { setUser({ ...user, passWord: event.target.value }) }} />
+        <button onClick={signUpUser}>Sign Up</button>
+      </div>
+    </>
+
   )
 }
 
